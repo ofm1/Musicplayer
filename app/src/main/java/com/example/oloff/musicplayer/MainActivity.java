02187a4e3f6df62bb90a6d6a3a537b292bf44be8
ViktorJH <vjh2@hi.is>
2018-11-24 23:47:54 +0000

    Virkja einn takka í main og back takka

--- app/src/main/java/com/example/oloff/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/oloff/musicplayer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.oloff.musicplayer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -64,6 +66,11 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+
+    public void openYourSongs(View view) {
+        Intent intent = new Intent(this, MusicListActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
